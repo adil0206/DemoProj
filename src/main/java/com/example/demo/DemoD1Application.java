5b320bd45060929b6f7eeb03dbcf44fb2ddbba6a
lenovo <nit.adilkhan0206@gmail.com>
2022-07-23 12:23:28 +0530

    My frirst commit by TL

---
+++ src/main/java/com/example/demo/DemoD1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoD1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoD1Application.class, args);
+	}
+
+}
